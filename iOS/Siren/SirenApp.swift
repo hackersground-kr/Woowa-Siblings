9d6e15b70ef464f7a01dfad86741ea148da7a6d0
Mercen <mercen@mercen.net>
2023-06-21 20:47:02 +0900

    FIX: Project Directory

---
+++ iOS/Siren/SirenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SirenApp.swift
+//  Siren
+//
+//  Created by Mercen on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SirenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
