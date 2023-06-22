462a32360674c21282263bfe1480ec7bbb7e8bf9
Mercen <mercen@mercen.net>
2023-06-23 00:45:28 +0900

    ADD: Images

--- iOS/Siren/Sources/View/Map/MapView.swift
+++ iOS/Siren/Sources/View/Map/MapView.swift
@@ -13,8 +13,10 @@ struct MapView: UIViewRepresentable {
     let coordinates: [Coordinate]
     
     func makeUIView(context: Context) -> UIView {
+        let view = UIView()
         let mapView = NMFMapView(frame: .infinite)
-        return mapView
+        view.addSubview(mapView)
+        return view
     }
 
     func updateUIView(_ uiViewController: UIView, context: Context) {
