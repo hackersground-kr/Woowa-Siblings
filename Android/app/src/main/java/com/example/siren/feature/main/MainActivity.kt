5afb89aa3d22505011feec640bdf435224aba6d7
윤석규 <greeentea.dev@gmail.com>
2023-06-22 12:24:14 +0900

    Fix : ViewPager Bug

--- Android/app/src/main/java/com/example/siren/feature/main/MainActivity.kt
+++ Android/app/src/main/java/com/example/siren/feature/main/MainActivity.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.marginBottom
+import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.example.siren.R
@@ -24,7 +26,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initViewPager()
-//        binding.viewPager.setPageTransformer(pageTransformer)
 
 
         initMapView(savedInstanceState)
@@ -36,34 +37,29 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-    private fun initViewPager() {
-        // MyRecyclerViewAdapter is an standard RecyclerView.Adapter :)
-        binding.viewPager.adapter = MainAdapter(pagerDummy)
-        binding.viewPager.clipToPadding = false
-        binding.viewPager.clipChildren = false
-// You need to retain one page on each side so that the next and previous items are visible
-        binding.viewPager.offscreenPageLimit = 1
-        binding.viewPager.setPadding(80, 0, 80, 35)
-// Add a PageTransformer that translates the next and previous items horizontally
-// towards the center of the screen, which makes them visible
+    private fun initViewPager() = with(binding.viewPager) {
+        adapter = MainAdapter(pagerDummy)
+
+        offscreenPageLimit = 3
+        setPadding(50, 0, 50, 100)
+
         val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
-        val currentItemHorizontalMarginPx =
-            resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
         val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
-        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+
+        setPageTransformer { page, position ->
             page.translationX = -pageTranslationX * position
-            // Next line scales the item's height. You can remove it if you don't want this effect
-            page.translationY = 2 - (0.5f * abs(position))
-            // If you want a fading effect uncomment the next line:
-            // page.alpha = 0.25f + (1 - abs(position))
+            page.scaleY = 1 - (0.25f * kotlin.math.abs(position))
+
         }
-        binding.viewPager.setPageTransformer(pageTransformer)
+
         val itemDecoration = HorizontalMarginItemDecoration(
-            this,
+            context,
             R.dimen.viewpager_current_item_horizontal_margin
         )
-        binding.viewPager.addItemDecoration(itemDecoration)
+        addItemDecoration(itemDecoration)
     }
+
 
     private fun initMapView(savedInstanceState: Bundle?) {
         binding.mapView.onCreate(savedInstanceState)
